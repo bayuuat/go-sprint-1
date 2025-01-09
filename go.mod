19aa77b6d667de2298ca53802d9dfea71bf73bab
vendot <fikrialjauzi1@gmail.com>
2025-01-09 15:07:10 +0700

    post employee

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.21.5
 
 require (
 	github.com/doug-martin/goqu/v9 v9.19.0
+	github.com/go-playground/validator/v10 v10.23.0
+	github.com/gofiber/contrib/jwt v1.0.10
 	github.com/gofiber/fiber/v2 v2.52.5
 	github.com/golang-jwt/jwt/v5 v5.2.1
 	github.com/google/uuid v1.5.0
@@ -18,9 +20,6 @@ require (
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
-	github.com/go-playground/validator/v10 v10.23.0 // indirect
-	github.com/gofiber/contrib/jwt v1.0.10 // indirect
-	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/klauspost/compress v1.17.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
--- internal/api/employee.go
+++ internal/api/employee.go
@@ -3,9 +3,15 @@ package api
 import (
 	"context"
 	"time"
+	"net/http"
+	"strconv"
+	"fmt"
+	"log"
 
 	"github.com/bayuuat/go-sprint-1/domain"
+	"github.com/bayuuat/go-sprint-1/dto"
 	"github.com/bayuuat/go-sprint-1/internal/middleware"
+	"github.com/bayuuat/go-sprint-1/internal/utils"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -42,9 +48,24 @@ func (da employeeApi) CreateEmployee(ctx *fiber.Ctx) error {
 	_, cancel := context.WithTimeout(ctx.Context(), 10*time.Second)
 	defer cancel()
 
-	// KERJAIN DISINI BANG
+	var req dto.EmployeeReq
+	if err := ctx.BodyParser(&req); err != nil {
+		return ctx.Status(http.StatusUnprocessableEntity).JSON(dto.NewErrorResponse("Invalid request:" + err.Error()))
+	}
 
-	return ctx.Status(400).JSON(fiber.Map{})
+	fails := utils.Validate(req)
+	if len(fails) > 0 {
+		return ctx.Status(http.StatusBadRequest).JSON(dto.NewErrorResponse("Invalid request"))
+	}
+
+	id, msg, err := da.employeeService.CreateEmployee(ctx.Context(), req, req.Name)
+	if err != nil {
+		log.Println(fmt.Sprintf("Error create employee: %v", err))
+		fmt.Println(err)
+		return ctx.Status(http.StatusBadRequest).JSON(dto.NewErrorResponse(strconv.Itoa(msg) + err.Error() + " disini"))
+	}
+
+	return ctx.Status(http.StatusCreated).JSON(dto.NewSuccessCreateResponse((strconv.Itoa(msg)), id))
 }
 
 func (da employeeApi) UpdatedEmployee(ctx *fiber.Ctx) error {
