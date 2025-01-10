60ea8cdffa2776d8d14cf469b1192377b61bfae1
bayuuat <bayuuat@gmail.com>
2025-01-10 14:07:41 +0700

    feat: add validation

--- internal/utils/validation.go
+++ internal/utils/validation.go
@@ -1,15 +1,39 @@
 package utils
 
 import (
+	"fmt"
+
+	"github.com/go-playground/locales/en"
+	ut "github.com/go-playground/universal-translator"
 	"github.com/go-playground/validator/v10"
+	enTranslations "github.com/go-playground/validator/v10/translations/en"
 )
 
+var (
+	uni      *ut.UniversalTranslator
+	validate *validator.Validate
+	trans    ut.Translator
+)
+
+func init() {
+	enLocale := en.New()
+	uni = ut.New(enLocale, enLocale)
+	trans, _ = uni.GetTranslator("en")
+	validate = validator.New()
+	enTranslations.RegisterDefaultTranslations(validate, trans)
+}
+
 func Validate[T any](data T) map[string]string {
-	err := validator.New().Struct(data)
+	err := validate.Struct(data)
+	if err == nil {
+		return nil
+	}
+
 	res := map[string]string{}
 	if err != nil {
+		fmt.Print(err)
 		for _, v := range err.(validator.ValidationErrors) {
-			res[v.StructField()] = v.Error()
+			res[v.StructField()] = v.Translate(trans)
 		}
 	}
 	return res
