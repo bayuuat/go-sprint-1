0a88ea7fa82c8c9d71b5e6484871e9dd3fd93a71
nesangcode <k124k3n@gmail.com>
2025-01-13 23:58:20 +0700

    fix: url validation

--- internal/middleware/url.go
+++ internal/middleware/url.go
@@ -4,6 +4,6 @@ import "regexp"
 
 func ValidateUrl(url string) (result bool) {
 	regexCom := "^https?:\\/\\/[a-z0-9]+(?:[-.][a-z0-9]+)*(?::[0-9]{1,5})?(?:\\/[^\\/\\r\\n]+)*\\.[a-z]{2,5}(?:[?#]\\S*)?$"
-	result, _ = regexp.MatchString(regexCom, "http://wwgle")
+	result, _ = regexp.MatchString(regexCom, url)
 	return
 }
